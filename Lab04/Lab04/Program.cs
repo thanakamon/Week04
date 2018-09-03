65bc1184833c7439976ed40effa81aa5af0ee941
thanakamon <thanakamon>
2018-09-03 22:21:50 +0700

    W04P03

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,8 +7,8 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine("World.");
         }
     }
 }
