08cad94ee4373c3bf6bcddd565aeeab536e2a34f
thanakamon <thanakamon>
2018-09-04 19:34:35 +0700

    W04P14

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -5,9 +5,17 @@ namespace RandomNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Namber: ");
+            int p = int.Parse(Console.ReadLine());
             Random random = new Random();
-            int randomNumber = random.Next(0, 100);
+            int randomNumber = random.Next(0, 9);
             Console.WriteLine(randomNumber);
+           
+
+            bool a = randomNumber == p;
+            Console.WriteLine("{0}", a);
+
+
         }
     }
 }
