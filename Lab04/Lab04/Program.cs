fa2dfb32a5491c9367a2488957b6533a7b3f2a92
thanakamon <thanakamon>
2018-09-04 18:00:12 +0700

    W04P11

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -8,15 +8,9 @@ namespace Lab04
         static void Main(string[] args)
         {
             string str;
-            Console.Write("Enter some characters.");
-            str = Console.Read();
-            Console.WriteLine("You entered: " + str);
-           
-
-
-
-
-
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
 
         }
     }
