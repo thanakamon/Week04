f9532bf02561170e8aec0bdbb31a0cd57fd30eaa
thanakamon <thanakamon>
2018-09-04 19:25:49 +0700

    W04P13

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,28 +1,16 @@
 ﻿using System;
-
-
-namespace Lab04
+namespace RandomNumber
 {
     class Program
     {
         static void Main(string[] args)
         {
-            {
-                try
-                {
-                    Console.Write("Please enter value 1 :");
-                    int val1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Please enter value 2 :");
-                    int val2 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Answer = " + (val1 + val2));
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error : " + e.ToString());
-                }
-            }
-
-
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+            Console.WriteLine(randomNumber);
         }
     }
 }
+
+
+
